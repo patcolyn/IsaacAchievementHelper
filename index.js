66c530b33942f5575b3d90b7b78d5929c9bb44ef
Pat Colyn <patcolyn@gmail.com>
2025-07-31 07:52:07 +0100

    Rewrite

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import AchievementHelper from './app/AchievementHelper.js';
 
-async function init() {
-  try {
-    const response = await fetch('./app/data/config.json');
-    if (!response.ok) throw new Error('Failed to load config.json');
-    const config = await response.json();
-
-    new AchievementHelper(config);
-  } catch (error) {
-    console.error('Error loading config:', error);
-  }
-}
-
-init();
+(async () => {
+    try {
+        const res = await fetch('./app/data/config.json');
+        if (!res.ok) throw new Error('Failed to load config.json');
+        const config = await res.json();
+        new AchievementHelper(config);
+    } catch (err) {
+        console.error('Error loading config:', err);
+    }
+})();
