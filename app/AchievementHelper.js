927b992c6d168041b68b83236b3ecbfdddb8e7d1
Pat Colyn <patcolyn@gmail.com>
2025-08-04 15:22:27 +0100

    Adjust icon padding

--- app/AchievementHelper.js
+++ app/AchievementHelper.js
@@ -168,7 +168,7 @@ export default class AchievementHelper {
                 .replace(/<img:(\w+)>/g, (_, key) => {
                     const url = this.tooltipImages[key];
                     return url
-                    ? `<img src="${url}" alt="${key}" style="height:0.9em; vertical-align:baseline; margin:0 2px;">`
+                    ? `<img src="${url}" alt="${key}" style="height:0.9em; vertical-align:baseline; margin:0 0px;">`
                     : '';
 
 
